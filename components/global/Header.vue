9654f6564c12d790f51c701872948ebb8d35ac3c
sumoncse19 <git-sumon@devxhub.com>
2023-12-26 20:17:27 +0600

    add modal

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -140,6 +140,8 @@ const setCurrentNestedMenu = (activeNested) => {
     currentNestedMenu.value = null;
   }
 };
+
+const showModal = ref(false);
 </script>
 
 <template>
@@ -277,14 +279,18 @@ const setCurrentNestedMenu = (activeNested) => {
               <span>+880 1318 116458</span>
             </a>
           </li>
-          <li>
-            <NuxtLink
-              to="/contact-us"
-              aria-label="Get a quote"
-              class="flex justify-center items-center w-[175px] px-[20px] py-[14px] h-14 xl:h-[56px] bg-primary font-bold text-[#1A1139] text-lg rounded-sm whitespace-nowrap"
+          <li @click="showModal = !showModal">
+            <div
+              class="flex justify-center items-center w-[175px] px-[20px] py-[14px] h-14 xl:h-[56px] bg-primary font-bold text-[#1A1139] text-lg rounded-sm whitespace-nowrap cursor-pointer"
             >
+              <!-- <NuxtLink
+                to="/contact-us"
+                aria-label="Get a quote"
+                
+              > -->
               <span>Get a Quote</span>
-            </NuxtLink>
+              <!-- </NuxtLink> -->
+            </div>
           </li>
           <li class="hidden">
             <NuxtLink to="/appointment" aria-label="Schedule a meeting"
@@ -501,6 +507,49 @@ const setCurrentNestedMenu = (activeNested) => {
                 >
               </li>
             </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- <BaseModal /> -->
+
+      <div
+        v-if="showModal"
+        class="fixed z-10 overflow-y-auto top-0 w-full left-0"
+      >
+        <div
+          class="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        >
+          <div class="fixed inset-0 transition-opacity">
+            <div class="absolute inset-0 bg-gray-900 opacity-75" />
+          </div>
+          <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
+            >&#8203;</span
+          >
+          <div
+            class="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+          >
+            <div>
+              <a
+                href="tel:+8801318-116458"
+                aria-label="Phone Number"
+                class="flex justify-center items-center px-[20px] py-[14px] h-14 xl:h-[56px] border border-primary font-bold text-[#FFF] text-lg rounded-sm whitespace-nowrap bg-gray-400"
+              >
+                <span>+880 1318 116458</span>
+              </a>
+            </div>
+            <div class="bg-gray-200 px-4 py-3 text-right">
+              <button
+                type="button"
+                class="py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-700 mr-2"
+                @click="showModal = !showModal"
+              >
+                <i class="fas fa-times"></i> Cancel
+              </button>
+            </div>
           </div>
         </div>
       </div>
